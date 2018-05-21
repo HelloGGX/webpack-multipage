604e6a4a9e8b6bfd6d7b07f94b9b98f87abc57a9
ggx <1056937524@qq.com>
2018-05-21 17:57:38 +0800

    完善俱乐部详情信息获取

--- src/pages/club-detail/club-detail.js
+++ src/pages/club-detail/club-detail.js
@@ -17,22 +17,93 @@ let all = (function () {
         defaultIndex: 0,
         onChange: (index) => {
           if (index === 1) {
-            this._acts()
+            this._getActs()
+          } else if (index === 2) {
+            this._getAlbums()
+          } else if (index === 3) {
+            this._getNews()
           }
         }
       })
     },
-    _acts () {
+    _getNews () { // 获取游记信息
+      let newdata
+      let _html = ''
+      newdata = DATA['news']
+      let len = newdata.length
+      for (let i = 0; i < len; i++) {
+        _html += this._newTemp(i, newdata)
+      }
+      $('#news').html(_html)
+    },
+    _newTemp (i, data) { // 游记模板
+      return `<div class="store-item" data-id="${data[i].id}">
+      <a href="">
+      <div class="store-content">
+          <div class="goods-image">
+              <div class="image-container">
+                  <img src="${data[i].thumb}" alt="">
+              </div>
+          </div>
+          <div class="goods-detail">
+              <p class="goods-name">${data[i].title}</p>
+              <div class="goods-content">
+                  <p class="goods-sales">更新时间${data[i].release_time}</p>
+              </div>
+              <div class="author">
+                  <div class="author-head">
+                      <img src="${data[i].author_thumb}" alt="">
+                  </div>
+                  <div class="author-name">
+                      <p>${data[i].author_name}</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </a>
+  </div>`
+    },
+    _getAlbums () { // 获取相册信息
+      let newdata
+      let _html = ''
+      newdata = DATA['albums']
+
+      let len = newdata.length
+      for (let i = 0; i < len; i++) {
+        _html += this._albumsTemp(i, newdata)
+      }
+      if ($('.tidbits-con .col-50').length > 1) {
+        $('#addAlbums').parent().nextAll().html(_html)
+      } else {
+        $('#addAlbums').parent().after(_html)
+      }
+    },
+    _albumsTemp (i, data) { // 活动相册模板
+      return `<div class="col-50" data-id="${data[i].id}" style="margin-top: 0.1rem;">
+      <a href="#">
+          <div class="tidbits-item">
+              <div class="tidbits-img">
+                  <img src="${data[i].photos[0].url[0]}" alt="">
+              </div>
+              <div class="tidbits-inner">
+                  <p class="f-s">${data[i].name}</p>
+                  <p class="f-s text-right">${data[i].photos_num}张</p>
+              </div>
+          </div>
+      </a>
+  </div>`
+    },
+    _getActs () { // 获取活动列表信息
       let newdata
       let _html = ''
       newdata = DATA['club_acts']
       let len = newdata.length
       for (let i = 0; i < len; i++) {
-        _html += this._temple(i, newdata)
+        _html += this._actTemple(i, newdata)
       }
       $('#acts').html(_html)
     },
-    _temple (i, data) { // 模板
+    _actTemple (i, data) { // 模板
       return `<div class="store-item" data-id=${data[i].id}>
             <a href="act-detail.html?id=${data[i].id}">
             <div class="store-content">
@@ -46,7 +117,7 @@ let all = (function () {
                     <div class="goods-content">
                         <p class="goods-sales">活动时间${data[i].time}</p>
                     </div>
-                    <del class="goods-market-price">${data[i].price}</del>
+                    <div class="goods-market-price">报名人数:${data[i].apply_num}</div>
                     <div class="discount-price"><i>￥</i>${data[i].price}</div>
                     <div class="goods-buy">立即查看</div>
                 </div>
